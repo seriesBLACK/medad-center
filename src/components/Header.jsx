fcc68304b1752323e4f95bd044da802a8ee02cd7
Abd ul Walee Talal <aboodtalal51@gmail.com>
2024-04-14 21:16:06 +0300

    make hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,22 @@ export default function Header() {
   return (
     <header>
 
-      <button>تواصل معنا</button>
+      <button className="button">تواصل معنا</button>
 
 
-      <div class="header-img-search">
+      <div className="header-img-search">
         <nav>
           <a href="">عن مداد</a>
           <a href="">الخدمات</a>
           <a href="">أعمالنا</a>
         </nav>
 
-        <div class="search-container">
+        <div className="search-container">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="100">
             <path
               d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
           </svg>
-          <input type="text" class="search" placeholder="...ابحث" />
+          <input type="text" className="search" placeholder="...ابحث" />
         </div>
 
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,75 @@
+import "../css/hero.css";
+
+import abs from "/partrners/abs.png";
+import aden from "/partrners/aden.png";
+import adra from "/partrners/adra.png";
+import fmfcr from "/partrners/fmfcr.png";
+import ithad from "/partrners/ithad.png";
+import msahah from "/partrners/msahah.png";
+import nahdah from "/partrners/abs.png";
+
+export default function Hero() {
+
+  const imags = [
+    {
+      id: 1,
+      img: abs
+    },
+    {
+      id: 2,
+      img: aden
+    },
+    {
+      id: 3,
+      img: adra
+    },
+    {
+      id: 4,
+      img: fmfcr
+    },
+    {
+      id: 5,
+      img: ithad
+    },
+    {
+      id: 6,
+      img: msahah
+    },
+    {
+      id: 7,
+      img: nahdah
+    }
+  ]
+
+
+  return (
+    <main>
+      <div className="hero-container">
+
+
+        <div className="imageContainer">
+          <img src="offers.png" alt="" />
+        </div>
+
+        <div className="hero-wrapper">
+          <p className="">في مركز<span style={{ color: "#fbbf5f" }}> مداد</span></p>
+          <h2>كل ماتحتاجه في عالم الاعلان والمطبوعات</h2>
+          <h6>بفريق من خبراء التصميم نحرص على دقة وجودة منجاتنا</h6>
+          <div className="buttons">
+
+            <a href="#projects"><button className="button">أبرز خدماتنـا</button></a>
+          </div>
+
+        </div>
+      </div>
+
+      <p className="text-[#fbbf5f] font-[Harmattan] text-[25px]">شركاء مداد</p>
+      <div className="parnters">
+        {imags.map((img) => (
+          <img key={img.id} src={img.img} alt="" />
+        ))}
+      </div>
+
+    </main>
+  )
+}
